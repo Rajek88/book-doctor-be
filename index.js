b393619c7e9303840a83ed00c444866f61a35214
Rajendra Kulkarni <rajerkulkarni01@gmail.com>
2024-11-24 01:51:43 +0530

    aded home route

--- index.js
+++ index.js
@@ -7,5 +7,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/appointments", appointmentRoutes);
+app.get("/", (req, res) => {
+  return res.status(200).send({
+    message: "Yes! I am online",
+  });
+});
 
 module.exports = app;
